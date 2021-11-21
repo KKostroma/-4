1286f6fd2c7bd9e40ec30b56857b39a1d734c59b
Ekaterina Kostromitskaia <kostroma.mi@mail.ru>
2021-11-21 15:53:11 +0300

    Финализация проекта

--- Задание 4/Program.cs
+++ Задание 4/Program.cs
@@ -10,7 +10,13 @@ namespace Задание_4
     {
         static void Main(string[] args)
         {
-            double
+            int N2 = 0;
+            int N = Convert.ToInt32(Console.ReadLine());
+                for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine("Квадрат числа {0} равен {1}", i, N2 = N2 + (2 * i - 1));
+            }
+            Console.ReadKey();
         }
     }
 }
